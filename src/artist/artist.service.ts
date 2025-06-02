ccc37df78103928d114c8a386988da3058dbda34
Irina Stepanova <arumirinka@github.com>
2025-06-03 05:12:03 +0600

    feat: add artists functionality

---
+++ src/artist/artist.service.ts
@@ -0,0 +1,50 @@
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { UpdateArtistDto } from './dto/update-artist.dto';
+import { DatabaseService } from 'src/database/database.service';
+import { v4 } from 'uuid';
+
+@Injectable()
+export class ArtistService {
+  constructor(private database: DatabaseService) {}
+
+  create(createArtistDto: CreateArtistDto) {
+    const id = v4();
+    const newArtist = {
+      ...createArtistDto,
+      id,
+    };
+    this.database.artists.push(newArtist);
+    return newArtist;
+  }
+
+  findAll() {
+    return this.database.artists;
+  }
+
+  findOne(id: string) {
+    const artist = this.database.artists.find(artist => artist.id === id);
+    if (!artist) throw new NotFoundException("Artist not found");
+    return artist;
+  }
+
+  update(id: string, updateArtistDto: UpdateArtistDto) {
+    if (!id || !updateArtistDto.name || !updateArtistDto.hasOwnProperty("grammy")) throw new BadRequestException("Invalid input");
+    const artist = this.database.artists.find(artist => artist.id === id);
+    if (!artist) throw new NotFoundException("Artist not found");
+
+    artist.name = updateArtistDto.name;
+    artist.grammy = updateArtistDto.grammy;
+
+    return artist;
+  }
+
+  remove(id: string) {
+    const artistIndex = this.database.artists.findIndex(artist => artist.id === id);
+    if (artistIndex < 0) throw new NotFoundException("Artist not found");
+    this.database.artists.splice(artistIndex, 1);
+    this.database.tracks.forEach(track => {
+      if (track.artistId === id) track.artistId = null;
+    })
+  }
+}
--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,4 +1,5 @@
 import { Global, Injectable } from '@nestjs/common';
+import { Artist } from 'src/artist/entities/artist.entity';
 import { Track } from 'src/track/entities/track.entity';
 import { User } from 'src/user/entities/user.entity';
 
@@ -7,9 +8,11 @@ import { User } from 'src/user/entities/user.entity';
 export class DatabaseService {
   public users: User[];
   public tracks: Track[];
+  public artists: Artist[];
 
   constructor() {
     this.users = [];
     this.tracks = [];
+    this.artists = [];
   }
 }
